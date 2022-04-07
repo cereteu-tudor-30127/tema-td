45ccd9513b8f13f8994d6424eef7ce9f1860be1a
cereteu-tudor-30127 <79508916+cereteu-tudor-30127@users.noreply.github.com>
2022-04-07 15:31:20 +0300

    Add files via upload

--- run.js
+++ run.js
@@ -1,21 +1,21 @@
-var api = require("./api").app;
-var hamming = require("./hamming");
-
-api.put("/message", function (request, response) {
+var api = require("./api.js").app;
+var hamming = require("./hamming.js");
+api.put("/ message ", function (request, response) {
   var bits = request.body.bits;
+  bits = distortBit(bits, 2);
 
   var decoded = hamming.decode(bits);
   if (decoded.errorCorrected) {
-    response.json("One error corrected on positon" + decoded.errorPosition);
+    response.json("One error corrected on position:" + decoded.errorPosition);
   }
   response.json("Message received without errors");
 });
 
 api.listen(3000, function () {
-  console.log("Server running on port 3000");
+  console.log("CORS - enabled web server is listening on port3000... ");
 });
 
-function disortBit(bits, index) {
+function distortBit(bits, index) {
   bits[index] = (bits[index] + 1) % 2;
   return bits;
 }
